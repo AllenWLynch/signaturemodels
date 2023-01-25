49d261ac2b724474b3d2d9612727b0f9fd817902
Lynch <all6739@compute-e-16-233.o2.rc.hms.harvard.edu>
2023-01-25 11:13:21 -0500

    added pip setup file

---
+++ locusregression/simulation/simulation.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class SimulatedCorpus:
+
+    markov_matrix = np.array([
+
+    ])
+
+    @staticmethod
+    def create(
+        n_cells = 100,
+        log_mean_mutations = 5,
+        log_std_mutations = 1.,
+        gamma_prior = 1.,
+        state_markov_matrix = SimulatedCorpus.markov_matrix,
+    ):
+        pass
+
+
+def simulate_corpus():
+    pass
