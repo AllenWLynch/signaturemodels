7867a9312cc14189b6d8c9fc07e72a30d44b3546
Allen W Lynch <allen.lynch1@outlook.com>
2023-10-29 17:20:18 -0400

    added new tuning files: objective and study

---
+++ locusregression/tuning/_objective.py
@@ -0,0 +1,58 @@
+
+from ..model import LocusRegressor
+from ..model import GBTRegressor
+import optuna
+from tqdm import trange
+
+def objective(trial,
+            min_components = 2, 
+            max_components = 10,
+            model_params = {},
+            tune_subsample = True,
+            locus_subsample_rates = [0.0625, 0.125, 0.25,],
+            model_type = 'regression',*,
+            num_epochs,
+            train, test,
+            ):
+    
+    
+    if model_type == 'regression':
+        basemodel = LocusRegressor
+    elif model_type == 'gbt':
+        basemodel = GBTRegressor
+    else:
+        raise ValueError(f'Unknown model type {model_type}')
+    
+    sample_params = basemodel.sample_params(trial)
+    sample_params['n_components'] = trial.suggest_int('n_components', min_components, max_components)
+
+    if tune_subsample:
+        sample_params['locus_subsample'] = trial.suggest_categorical('locus_subsample', locus_subsample_rates)
+        sample_params['batch_size'] = trial.suggest_categorical('batch_size', [32,64,128])
+
+    model_params.update(sample_params)
+    
+    model = basemodel(
+            eval_every = 1000000,
+            quiet=True,
+            **model_params,
+            seed = trial.number,
+            num_epochs=1,
+        )
+    
+    for i in trange(1, num_epochs + 1, desc = 'Training', ncols=100, 
+                        position=0, leave=True):
+        
+        model.num_epochs = i
+        model._fit(train, reinit = False)
+
+        if i % 25 == 0:
+            intermediate_score = model.score(test)
+            trial.report(intermediate_score, i)
+
+            if trial.should_prune():
+                raise optuna.TrialPruned()
+            
+    
+    return model.score(test)
+
---
+++ locusregression/tuning/study.py
@@ -0,0 +1,133 @@
+import optuna
+import os
+from ..corpus import stream_corpus, MetaCorpus, train_test_split
+from functools import partial
+from ._objective import objective
+
+
+def _get_nfs_storage(study_name):
+    
+    journal = os.path.join(
+                    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+                    f'.journal.{study_name}.db'
+                )
+
+    storage = optuna.storages.JournalStorage(
+        optuna.storages.JournalFileStorage(journal)
+    )
+    return storage
+
+
+
+def create_study(
+    locus_subsample = 0.125,
+    batch_size = 32,
+    factor = 3,
+    train_size = 0.7,
+    tune_subsample= False,
+    model_type = 'regression',
+    empirical_bayes = False,
+    pi_prior = 1.,
+    num_epochs = 500,
+    locus_subsample_rates = [0.0625, 0.125, 0.25],
+    storage = None,
+    seed = 0,*,
+    corpuses,
+    min_components, 
+    max_components,
+    study_name,
+):
+    
+    if storage is None:
+        storage = _get_nfs_storage(study_name)    
+
+    model_params = dict(
+        locus_subsample=locus_subsample,
+        batch_size = batch_size,
+        pi_prior = pi_prior,
+        empirical_bayes = empirical_bayes,
+    )
+
+    study = optuna.create_study(
+        study_name = study_name,
+        storage = storage,
+        direction = 'maximize',
+        load_if_exists = True,
+        pruner = optuna.pruners.HyperbandPruner(
+            min_resource = 50,
+            reduction_factor = factor,
+            max_resource = num_epochs,
+            bootstrap_count = 1,
+        ),
+        sampler = optuna.samplers.RandomSampler(
+            seed = seed,
+        ),
+    )
+
+    corpuses = [os.path.abspath(corpus) for corpus in corpuses]
+
+    study.set_user_attr('corpuses', corpuses)
+    study.set_user_attr('min_components', min_components)
+    study.set_user_attr('max_components', max_components)
+    study.set_user_attr('model_params', model_params)
+    study.set_user_attr('tune_subsample', tune_subsample)
+    study.set_user_attr('locus_subsample_rates', locus_subsample_rates)
+    study.set_user_attr('model_type', model_type)
+    study.set_user_attr('seed', seed)
+    study.set_user_attr('train_size', train_size)
+    study.set_user_attr('num_epochs', num_epochs)
+
+
+
+def load_study(study_name, storage = None):
+    
+    if storage is None:
+        storage = _get_nfs_storage(study_name)    
+
+    study = optuna.load_study(
+                    study_name=study_name, 
+                    storage=storage
+                    )
+
+    attrs = study.user_attrs
+    corpuses = attrs['corpuses']
+
+    if len(corpuses) == 1:
+        dataset = stream_corpus(corpuses[0])
+    else:
+        dataset = MetaCorpus(*[
+            stream_corpus(corpus) for corpus in corpuses
+        ])
+
+    return study, dataset, attrs
+
+
+
+def run_trial(*,study_name, iters, storage = None):
+
+    study, dataset, attrs = load_study(study_name, storage)
+
+    train, test = train_test_split(
+        dataset,
+        train_size=attrs['train_size'],
+        seed = attrs['seed'],
+    )
+    
+    obj_func = partial(
+        objective,
+        min_components = attrs['min_components'], 
+        max_components = attrs['max_components'],
+        model_params = attrs['model_params'],
+        tune_subsample = attrs['tune_subsample'],
+        locus_subsample_rates = attrs['locus_subsample_rates'],
+        model_type = attrs['model_type'],
+        num_epochs = attrs['num_epochs'],
+        train = train,
+        test = test,
+    )
+
+    study.optimize(
+        obj_func,
+        n_trials = iters,
+    )
+
