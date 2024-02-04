f0ed6c19b0bb3537e7bf60ea1d7c55a913ae3f4e
AllenWLynch <allen.lynch1@outlook.com>
2024-02-04 16:31:14 -0500

    moved cli functions to a _cli_utils, implementing rainfall-based filtering of per-sample kategous events

--- locusregression/cli.py
+++ locusregression/cli.py
@@ -1,11 +1,11 @@
+from ._cli_utils import *
 from .corpus import *
 from .corpus import logger as reader_logger
-from .model import LocusRegressor, load_model, logger, GBTRegressor
+from .model import load_model, logger
 from .model._importance_sampling import get_posterior_sample
 from .tuning import run_trial, create_study, load_study
 from .simulation import SimulatedCorpus, coef_l1_distance, signature_cosine_distance
 import argparse
-from argparse import ArgumentTypeError
 import os
 import sys
 import logging
@@ -16,149 +16,12 @@ import logging
 import warnings
 from matplotlib.pyplot import savefig
 from .explanation.explanation import explain
-from tempfile import NamedTemporaryFile
 from functools import partial
-import subprocess
 from joblib import Parallel, delayed
 import joblib
 
 from optuna.exceptions import ExperimentalWarning
 warnings.filterwarnings("ignore", category=ExperimentalWarning)
-
-
-def get_corpusstate_cache_path(model_path, corpus_path):
-    return os.path.join(
-        os.path.dirname(corpus_path),
-        '.' + os.path.basename(corpus_path) + os.path.relpath(model_path, corpus_path)\
-            .replace('/','.')\
-            .replace(' ','') \
-        + '.corpusstate'
-    )
-
-
-def load_corpusstate_cache(model_path, corpus_path):
-    
-    cache_path = get_corpusstate_cache_path(model_path, corpus_path)
-
-    model_mtime = os.path.getmtime(model_path)
-    corpus_mtime = os.path.getmtime(corpus_path)
-    cache_mtime = os.path.getmtime(cache_path)
-
-    assert model_mtime > corpus_mtime, 'Model must have been trained after corpus was last modified.'
-    assert cache_mtime > model_mtime, 'Cache must have been created after model was trained'
-
-    # check if model was saved after corpus, and if path was saved after model
-    return joblib.load(cache_path)
-
-
-
-def transfer_annotations_vcf(
-        annotations_df,*,
-        vcf_file,
-        description, 
-        output, 
-        chr_prefix=''
-    ):
-
-    annotations_df = annotations_df.copy()
-
-    annotations_df['CHROM'] = annotations_df.CHROM.str.removeprefix(chr_prefix)
-    annotations_df['POS'] = annotations_df.POS + 1 #switch to 1-based from 0-based indexing
-    annotations_df = annotations_df.sort_values(['CHROM','POS'])
-
-    transfer_columns = ','.join(['CHROM','POS'] + list(map(lambda c : 'INFO/' + c, annotations_df.columns[2:])))
-
-    with NamedTemporaryFile() as header, \
-        NamedTemporaryFile(delete=False) as dataframe:
-
-        with open(header.name, 'w') as f:
-            for col in annotations_df.columns[2:]:
-                print(
-                    f'##INFO=<ID={col},Number=1,Type=Float,Description="{description}">',
-                    file = f,
-                    sep = '\n',
-                )
-
-            annotations_df.to_csv(dataframe.name, index = None, sep = '\t', header = None)
-
-        try:    
-            subprocess.check_output(['bgzip','-f',dataframe.name])
-            subprocess.check_output(['tabix','-s1','-b2','-e2', '-f', dataframe.name + '.gz'])
-
-            subprocess.check_output(
-                ['bcftools','annotate',
-                '-a',  dataframe.name + '.gz',
-                '-h', header.name,
-                '-c', transfer_columns,
-                '-o', output,
-                vcf_file,
-                ]
-            )
-        
-        finally:
-            os.remove(dataframe.name + '.gz')
-            os.remove(dataframe.name + '.gz.tbi')
-
-
-def posint(x):
-    x = int(x)
-
-    if x > 0:
-        return x
-    else:
-        raise ArgumentTypeError('Must be positive integer.')
-
-
-def posfloat(x):
-    x = float(x)
-    if x > 0:
-        return x
-    else:
-        raise ArgumentTypeError('Must be positive float.')
-
-
-def file_exists(x):
-    if os.path.exists(x):
-        return x
-    else:
-        raise ArgumentTypeError('File {} does not exist.'.format(x))
-
-def valid_path(x):
-    if not os.access(x, os.W_OK):
-        
-        try:
-            open(x, 'w').close()
-            os.unlink(x)
-            return x
-        except OSError as err:
-            raise ArgumentTypeError('File {} cannot be written. Invalid path.'.format(x)) from err
-    
-    return x
-
-
-def _load_dataset(corpuses):
-
-    if len(corpuses) == 1:
-        dataset = stream_corpus(corpuses[0])
-    else:
-        dataset = MetaCorpus(*[
-            stream_corpus(corpus) for corpus in corpuses
-        ])
-
-    return dataset
-
-
-def _get_basemodel(model_type):
-
-    if model_type == 'linear':
-        basemodel = LocusRegressor
-    elif model_type == 'gbt':
-        basemodel = GBTRegressor
-    else:
-        raise ValueError(f'Unknown model type {model_type}')
-
-    return basemodel
-
 
 
 parser = argparse.ArgumentParser(
@@ -830,7 +693,7 @@ def _write_posterior_annotated_vcf(
                         n_iters=5000,
                     )
 
-    transfer_annotations_vcf(
+    transfer_annotations_to_vcf(
         posterior_df,
         vcf_file=input_vcf,
         description='Log-probability under the posterior that the mutation was generated by this process.', 
---
+++ locusregression/corpus/filtering.py
@@ -0,0 +1,60 @@
+import subprocess
+import tempfile
+
+def get_rainfall_statistic(vcf_file,*,output):
+
+    def get_distance_cutoff(mutations_file):
+        pass
+
+    
+    with tempfile.NamedTemporaryFile() as temp_file:
+
+        filter_process = subprocess.Popen(
+                ['bcftools','view','-v','snps', vcf_file],
+                stdout = subprocess.PIPE,
+                universal_newlines=True,
+                bufsize=10000,
+                stderr = subprocess.DEVNULL,
+            )
+        
+        with open(tempfile,'w') as f:
+            query_process = subprocess.Popen(
+                ['bcftools','query','-f', chr_prefix + '%CHROM\t%POS0\t%POS0' + '\n',],
+                stdin = filter_process.stdout,
+                stdout = f,
+                universal_newlines=True,
+                bufsize=10000,
+            )
+            query_process.wait()
+
+        closest_process = subprocess.Popen(
+            ['bedtools','closest',
+             '-a',temp_file.name,
+             '-b',temp_file.name,
+             '-io',
+            ],
+            stdout = subprocess.PIPE,
+            universal_newlines=True,
+            bufsize=10000,
+        )
+
+        cut_process = subprocess.Popen(
+            ['cut','-f1-3,7'],
+            stdin = closest_process.stdout,
+            stdout = subprocess.PIPE,
+            universal_newlines=True,
+            bufsize=10000,
+        )
+
+        distance_cutoff = get_distance_cutoff(temp_file.name)
+
+        for line in cut_process.stdout:
+            
+            chrom, start, end, distance = line.strip().split('\t')
+            distance = int(distance)
+
+            print(
+                chrom, start, end, distance, distance < distance_cutoff,
+                sep='\t',
+                file=output
+            )
